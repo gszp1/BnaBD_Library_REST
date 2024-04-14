691acb7c6cdd02844420039f31575f9939ad4a78
gszp1 <grzybekmateusz@poczta.fm>
2024-04-14 15:12:19 +0200

    Adds Publisher controller and service.

---
+++ src/main/java/org/example/library_rest/controllers/PublisherController.java
@@ -0,0 +1,48 @@
+package org.example.library_rest.controllers;
+
+import org.example.library_rest.model.Publisher;
+import org.example.library_rest.service.PublisherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/api/publishers")
+public class PublisherController {
+
+    private final PublisherService publisherService;
+
+    @Autowired
+    public PublisherController(PublisherService publisherService) {
+        this.publisherService = publisherService;
+    }
+
+    @GetMapping("/all")
+    public List<Publisher> getAllPublishers(){
+        return publisherService.getAllPublishers();
+    }
+
+    @GetMapping("/{id}")
+    public Publisher getPublisherById(@PathVariable("id") Long id){
+        Optional<Publisher> publisher = publisherService.getPublisher(id);
+        return publisher.orElse(null);
+    }
+
+    @PostMapping("/add")
+    public Publisher savePublisher(@RequestBody Publisher publisher){
+        return publisherService.savePublisher(publisher);
+    }
+
+    @DeleteMapping("/delete")
+    public void deletePublisher(@RequestParam("id") Long id){
+        publisherService.deletePublisher(id);
+    }
+
+    @PutMapping("/update")
+    public Publisher updatePublisher(@RequestBody Publisher publisher){
+        return publisherService.updatePublisher(publisher);
+    }
+}
---
+++ src/main/java/org/example/library_rest/service/PublisherService.java
@@ -0,0 +1,40 @@
+package org.example.library_rest.service;
+
+import org.example.library_rest.model.Publisher;
+import org.example.library_rest.repository.PublisherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PublisherService {
+
+    private final PublisherRepository publisherRepository;
+
+    @Autowired
+    public PublisherService(PublisherRepository publisherRepository) {
+        this.publisherRepository = publisherRepository;
+    }
+
+    public List<Publisher> getAllPublishers(){
+        return publisherRepository.findAll();
+    }
+
+    public Optional<Publisher> getPublisher(Long id){
+        return publisherRepository.findById(id);
+    }
+
+    public Publisher savePublisher(Publisher publisher){
+        return publisherRepository.save(publisher);
+    }
+
+    public void deletePublisher(Long id){
+        publisherRepository.deleteById(id);
+    }
+
+    public Publisher updatePublisher(Publisher publisher){
+        return publisherRepository.save(publisher);
+    }
+}
