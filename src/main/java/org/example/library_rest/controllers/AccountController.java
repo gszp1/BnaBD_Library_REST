167078c596c31f35b61645c2aadf91d891fd4a61
gszp1 <grzybekmateusz@poczta.fm>
2024-04-14 12:26:25 +0200

    Adds Account service and controller.

---
+++ src/main/java/org/example/library_rest/controllers/AccountController.java
@@ -0,0 +1,46 @@
+package org.example.library_rest.controllers;
+
+import org.example.library_rest.model.Account;
+import org.example.library_rest.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/all")
+    public List<Account> getAllAccounts() {
+        return accountService.getAllAccounts();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Account> getAccountById(@PathVariable("id") Long id) {
+        return accountService.getAccountById(id);
+    }
+
+    @PostMapping("/add")
+    public Account createAccount(@RequestBody Account account) {
+        return accountService.createAccount(account);
+    }
+
+    @PutMapping("/update")
+    public Account updateAccount(@RequestBody Account account) {
+        return accountService.updateAccount(account);
+    }
+
+    @DeleteMapping("/delete")
+    public void deleteAccount(@RequestParam("id") Long id) {
+        accountService.deleteAccount(id);
+    }
+}
---
+++ src/main/java/org/example/library_rest/service/AccountService.java
@@ -0,0 +1,40 @@
+package org.example.library_rest.service;
+
+import org.example.library_rest.model.Account;
+import org.example.library_rest.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public List<Account> getAllAccounts() {
+        return accountRepository.findAll();
+    }
+
+    public Optional<Account> getAccountById(Long id) {
+        return accountRepository.findById(id);
+    }
+
+    public Account createAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    public Account updateAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    public void deleteAccount(Long id) {
+        accountRepository.deleteById(id);
+    }
+}
